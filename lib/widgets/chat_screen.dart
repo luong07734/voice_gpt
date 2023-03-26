4c38107b401c63cbf6964ee686587818f0975b67
luong07734 <le.ky.luong@gmail.com>
2023-03-26 12:03:41 +0700

    Text chatbot using chatGPT API
    - chat screen
    - chat GPT API

---
+++ lib/widgets/chat_screen.dart
@@ -0,0 +1,80 @@
+// chat_page.dart
+import 'package:flutter/material.dart';
+import 'package:voice_gpt/data/apis/chat_apis.dart';
+import 'package:voice_gpt/models/chat_message.dart';
+import 'package:voice_gpt/widgets/components/message_bubble.dart';
+import 'package:voice_gpt/widgets/components/message_compose.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({
+    required this.chatApi,
+    super.key,
+  });
+
+  final ChatApi chatApi;
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _messages = <ChatMessage>[
+    ChatMessage('Hello, how can I help?', false),
+  ];
+
+  var _awaitingResponse = false;
+
+  Future<void> _onSubmitted(String message) async {
+    setState(() {
+      _messages.add(ChatMessage(message, true));
+      _awaitingResponse = true;
+    });
+    try {
+      final response;
+      if (_messages.length < 6) {
+        response = await widget.chatApi.completeChat(_messages);
+      } else {
+        response = await widget.chatApi
+            .completeChat(_messages.sublist(_messages.length - 5));
+      }
+      setState(() {
+        _messages.add(ChatMessage(response, false));
+        _awaitingResponse = false;
+      });
+    } catch (err) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('An error occurred. Please try again.')),
+      );
+      setState(() {
+        _awaitingResponse = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                ..._messages.map(
+                  (msg) => MessageBubble(
+                    content: msg.content,
+                    isUserMessage: msg.isUserMessage,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          MessageComposer(
+            onSubmitted: _onSubmitted,
+            awaitingResponse: _awaitingResponse,
+          ),
+        ],
+      ),
+    );
+  }
+}
